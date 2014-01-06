9a60e44c3a1ecc9bad3251672e68f62ff594f2aa
Michael Kane <kaneplusplus@gmail.com>
2014-01-06 09:16:46 -0800

    Updated the runSnbDistApp for color filling of mass contribution.

--- inst/distApp/server.R
+++ inst/distApp/server.R
@@ -6,8 +6,7 @@ shinyServer(function(input, output) {
   output$plot <- renderPlot({
     if (!is.na(input$sParam) && !is.na(input$tParam)) {
       x <- input$sParam:(input$tParam+input$sParam-1)
-      p <- qplot(factor(x), dsnb(x, input$pParam, input$sParam, input$tParam),
-        stat="identity", geom="bar", ylab="f(t)", xlab="t")
+      p <- snm:::dsnbStackPlot(x, input$pParam, input$sParam, input$tParam)
       print(p)
     }
   }, height=600)
