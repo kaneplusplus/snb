4689e1e104687f8b6a46d3456e88e6585e424c21
Michael Kane <kaneplusplus@gmail.com>
2015-02-21 15:48:22 -0500

    The distribution needs to be normalized.

--- R/snb_dist.r
+++ R/snb_dist.r
@@ -26,13 +26,13 @@ dsnb_private_stacked = function(x, p, s, t, tol=1e-7) {
   b = foreach(k=1:(s+t-1), .combine=c) %do% R(k, p, t)
   d = a + b
   if (abs(sum(a+b) - 1) > tol) stop("Density error")
-  #u = a/sum(d)
-  #r = b/sum(d)
+  u = a/sum(d)
+  r = b/sum(d)
   ret = foreach (i=x, .combine=rbind) %do% {
     v = c(i, 0, 0)
     if (i %in% 1:length(d)) {
-      v[2] = a[i] #u[i] 
-      v[3] = r[i] #r[i] 
+      v[2] = u[i] 
+      v[3] = r[i] 
     }
     v
   }
